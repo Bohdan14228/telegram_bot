afc018b0f9a9297e27e26bafb645b3f554d92a7d
work <work@gmail.com>
2023-06-27 14:42:00 +0300

    func send_user_id, base

--- telegram_bot_project/project/bot.py
+++ telegram_bot_project/project/bot.py
@@ -1,6 +1,7 @@
 from aiogram import Bot, Dispatcher, executor, types
 from aiogram.dispatcher.filters import Text
 import random
+from keyboard import *
 
 API_TOKEN = '5993455599:AAF5T1T_U0Mgglb7aCMJLXQfnXRaW8Zt-_U'
 
@@ -13,14 +14,20 @@ async def on_startup(_):
 
 
 @dp.message_handler(commands=['start'])
-async def fist(message: types.Message):
+async def start(message: types.Message):
     if message.from_user.last_name is None:
         name = f"{message.from_user.first_name}"
     else:
         name = f"{message.from_user.first_name} {message.from_user.last_name}"
     mess = f'Привіт, {name}'
-    await message.answer(text=mess, reply_markup=kb)
+    await message.answer(text=mess, reply_markup=kb1)
     await message.delete()
+
+
+@dp.message_handler()
+async def fist(message: types.Message):
+    if message.text == 'Твій user_id':
+        await message.answer(message.from_user.id)
 
 
 if __name__ == '__main__':
--- telegram_bot_project/project/keyboard.py
+++ telegram_bot_project/project/keyboard.py
@@ -1,16 +1,17 @@
 from aiogram.types import *
 
-kb = ReplyKeyboardMarkup(resize_keyboard=True)
-b1 = KeyboardButton('/help')
-b2 = KeyboardButton('/description')
-b3 = KeyboardButton('photo')
-kb.add(b1, b2).add(b3)
+kb1 = ReplyKeyboardMarkup(resize_keyboard=True)
+b1 = KeyboardButton('Твій user_id')
+b2 = KeyboardButton('Адмін-панель')
+
+# b3 = KeyboardButton('photo')
+kb1.add(b1, b2)
 
 
-kb_photo = ReplyKeyboardMarkup(resize_keyboard=True)
-bp1 = KeyboardButton('Рандом')
-bp2 = KeyboardButton('Главное меню')
-kb_photo.add(bp1, bp2)
+# kb_photo = ReplyKeyboardMarkup(resize_keyboard=True)
+# bp1 = KeyboardButton('Рандом')
+# bp2 = KeyboardButton('Главное меню')
+# kb_photo.add(bp1, bp2)
 
 
 ikb = InlineKeyboardMarkup(row_width=2)
@@ -19,3 +20,9 @@ ib2 = InlineKeyboardButton(text='👎️', callback_data='dislike')
 ib3 = InlineKeyboardButton(text='Следующая фотка', callback_data='next')
 ib4 = InlineKeyboardButton(text='Главное меню', callback_data='main')
 ikb.add(ib1, ib2, ib3).add(ib4)
+
+
+# @dp.message_handler()
+# async def fist(message: types.Message):
+#     if message.text == 'Твій user_id':
+#         await bot.send_message(chat_id=message.from_user.id, text=f"{message.from_user.id}")
